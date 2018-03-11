e45c1ef7668b2804a17a9ed1f6900449c63578bb
maciej.wolejszo <maciej.wolejszo@gmail.com>
2018-03-11 15:41:29 +0100

    Lesson 03 - Generic Classes and Intefaces; Module: A Buffer interface;

---
+++ DataStructures/DataStructures.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DataStructures.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DataStructures.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("703dc7ea-13f6-4a50-bc78-a3ae7838e83c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
